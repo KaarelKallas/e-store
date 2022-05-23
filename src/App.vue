651bed7132b0d27f8c8f3b93a2391401e85c85b6
Carlos <Carlos.tuuling@ametikool.ee>
2022-05-23 11:33:31 +0300

    im bad at this

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import { RouterLink, RouterView } from 'vue-router'
  import { reactive, watch } from '@vue/runtime-core';
+ import ProductPage from "./views/ProductPage.vue"
 const products = reactive({
     data: {}
 })
--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -1,5 +1,18 @@
+<script setup>
+import axios from 'axios';
+ import { reactive, watch } from '@vue/runtime-core';
+const products = reactive({
+    data: {}
+})
+axios.get('https://fakestoreapi.com/products/1')
+    .then(response => products.data = response.data)
+</script>
+
 <template>
-<div class="flex justify-end ">
-<div class="bg-[#E5DFA0] h-[812px] w-[193px]"></div>
+<div><p>{{products.data.description}}</p></div>
+<div class="flex justify-end">
+<div class="bg-[#E5DFA0] h-[812px] w-[193px]">
+    <img :src="products.data.image" alt="">
+</div>
 </div>
 </template>
