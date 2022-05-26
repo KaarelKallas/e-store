bea4f7e1b1d30fd744397b4fd97ccfa7d667e613
PopeFranci5 <rihardpargma@gmail.com>
2022-05-26 12:45:00 +0300

    made grid alternate

--- src/components/ProductComponent.vue
+++ src/components/ProductComponent.vue
@@ -1,7 +1,7 @@
 <template>
-    <img :src="item.image" alt="" class="w-24 h-auto absolute z-10 bottom-[40px] left-[25px]">
-    <div class="w-[150px] h-[150px] bg-[#E5DFA0] absolute bottom-0">
-    
+    <img :src="item.image" alt="" class="w-[100px] h-auto absolute z-10 bottom-[57px] left-[25px]">
+    <div class="w-[150px] h-[150px] bg-[#E5DFA0] absolute bottom-0 flex items-end justify-center">
+        <p class="z-20 mb-[7px] text-black text-[10px] leading-[13px] font-[Acme] text-center w-[100px]">{{item.title}}</p>
     </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,11 +12,20 @@ axios.get('https://fakestoreapi.com/products')
 </script>
 
 <template>
-<div class="grid grid-cols-2 gap-x-[100px] justify-items-center items-center">
-    <div
-    v-for="(item, index) in products.data"
-    class="w-[150px] h-[250px]">
-        <ProductComponent :item="item"/>
+<div class="flex gap-x-[30px] justify-center">
+    <div>
+        <div
+        v-for="(item, index) in products.data.slice(0,6)"
+        class="w-[150px] h-[254px]">
+            <ProductComponent :item="item"/>
+        </div>
+    </div>
+    <div class="mt-[75px]">
+        <div
+        v-for="(item, index) in products.data.slice(6,12)"
+        class="w-[150px] h-[254px]">
+            <ProductComponent :item="item"/>
+        </div>
     </div>
 </div>
 </template>
