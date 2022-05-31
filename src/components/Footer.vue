084c128e6867c0c251fbcf28c51da32a868591b8
Kaarel Kallas <kaarel.kallas@ametikool.ee>
2022-05-31 10:34:52 +0300

    bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-black w-[100%] opacity-80 h-16 flex justify-between px-6">
+    <div class="bg-black w-[100%] opacity-80 h-16 flex justify-between px-6 absolute bottom-0">
         <img class="w-6" src="../assets/images/home.svg" alt="">
         <img class="w-6" src="../assets/images/heart.svg" alt="">
         <img class="w-6" src="../assets/images/search.svg" alt="">
