818454b6693b75efff98089fd20cb6a43143ddf4
Kaarel Kallas <kaarel.kallas@ametikool.ee>
2022-05-31 10:08:27 +0300

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-black w-[100%] opacity-80 h-16 flex justify-between px-6">
+        <img class="w-6" src="../assets/images/home.svg" alt="">
+        <img class="w-6" src="../assets/images/heart.svg" alt="">
+        <img class="w-6" src="../assets/images/search.svg" alt="">
+    </div>
+</template>
