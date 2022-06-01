11c17661b975eb32f4c1ae4d712d067917d70870
PopeFranci5 <rihardpargma@gmail.com>
2022-06-01 09:17:44 +0300

    added M A T H S

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -27,12 +27,14 @@ axios.get('https://fakestoreapi.com' + pagepath)
       <p class="w-[132px] text-[#FFEADC] text-[15px]">{{products.data.title}}</p>
       <p class="mt-[73px] text-[#FFEADC] text-[18px]">VÄNDA KAAREL OÜ</p>
       <p class="mt-[27px] w-[132px] text-[#FFEADC] text-[15px]">{{products.data.description}}</p>
-      <input type="radio" value="One" v-model="picked" @click="picked = 'One'"/>
-      <label class="text-[15px] text-[#FFEADC]" for="one">M</label>
-      <input type="radio" value="Two" v-model="picked" @click="picked = 'Two'" />
-      <label class="text-[15px] text-[#FFEADC]" for="two">L</label>
-      <p v-if="picked = 'One'" class="text-[25px] mt-[52px] text-[#FFEADC]">{{products.data.price}}$</p>
-      <!-- <p v-if="picked = 'Two'" class="text-[25px] mt-[52px] text-[#FFEADC]">{{products.data.price}}$</p> -->
+      <form action="">
+        <input type="radio" id="one" value="One" v-model="picked" @click="picked = 'One'"/>
+        <label class="text-[15px] text-[#FFEADC]" for="one">M</label>
+        <input type="radio" id="two" value="Two" v-model="picked" @click="picked = 'Two'" />
+        <label class="text-[15px] text-[#FFEADC]" for="two">L</label>
+      </form>
+      <p v-if="picked == 'One'" class="text-[25px] mt-[52px] text-[#FFEADC]">{{products.data.price}}€</p>
+      <p v-if="picked == 'Two'" class="text-[25px] mt-[52px] text-[#FFEADC]">{{products.data.price*2}}€</p>
       <button class="mt-[36px] bg-[#E5DFA0] text-[#000000] text-[15px] w-[114px] h-[40px]">BUY NOW</button>
     </div>
   </div>
